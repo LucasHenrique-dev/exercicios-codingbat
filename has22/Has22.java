8bf99097c47c2946429aed5cc7e1343e04d4691b
Lucas Henrique Santos Neto <lucas.hneto@hotmail.com>
2020-07-03 09:02:59 -0300

    Create Has22.java

---
+++ has22/Has22.java
@@ -0,0 +1,12 @@
+package has22;
+
+public class Has22 {
+    //Retorne true se houver um "2" imediatamente seguido por outro "2", em qualquer lugar do Array dado ("nums").
+
+    public boolean has22(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == 2 && nums[i+1] == 2) return true;
+        }
+        return false;
+    }
+}
